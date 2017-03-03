2aae166533b7ffc76ac90be73c12e83a3cb3de49
Aman Vishnani <aman62@gmail.com>
2017-03-03 23:53:33 +0530

    Added Sign In feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-
+import {OnInit} from '@angular/core';
+import { BackandService } from '@backand/angular2-sdk';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  res: string;
+  username: string;
+  password: string;
+  constructor(private backand: BackandService) { }
+  getList(): void {
+    this.res = 'fetching objects...';
+    this.backand.object.getList('users').then((res: any) => {
+      this.res = `${JSON.stringify(res.data)} objects fetched`;
+      console.log(res);
+    })
+    .catch(err => console.log(err));
+  }
+  ngOnInit(): void {
+    this.backand.init({
+      appName: 'amanv',
+      anonymousToken: 'abbf0a5b-3289-4987-a884-d00130f72c02'
+    });
+    this.getList();
+ }
+ signIn()
+ {
+   this.backand.signin(this.username, this.password)
+        .then((val)=>{console.log(val)})
+        .catch(err => console.log(err));
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BackandService } from '@backand/angular2-sdk'
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [BackandService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
